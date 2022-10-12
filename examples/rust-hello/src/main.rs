6581efbc34e4fc44b11730ed4995885dfd4d550b
Valentin <valentin.lairich@gmail.com>
2022-10-12 19:43:38 +0200

    edit gitignore

--- examples/rust-hello/src/main.rs
+++ examples/rust-hello/src/main.rs
@@ -16,13 +16,22 @@ extern crate psoc6_hal;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;*/
 
+
 use cortex_m_rt::entry;
 use panic_halt as _;
+use cortex_m_semihosting::hprintln;
+
+/*
+use cortex_m::peripheral::{syst, Peripherals};
+use cortex_m_rt::entry;
+use panic_halt as _;*/
 
 
 /*#[psoc6_hal::entry]*/
 #[entry]
 fn main() -> ! {
     /*hprintln!("Hello, world! Rust").unwrap();*/
-    loop{}
+    loop{
+        hprintln!("Hello, world! Rust").unwrap();   
+    }
 }
